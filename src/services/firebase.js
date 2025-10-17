3a7b0cd485376d061d64925d45d1e087168677db
Yash Manek <yashmanek2001@gmail.com>
2025-10-18 00:31:56 +0530

    fix

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey:
+    process.env.REACT_APP_FIREBASE_API_KEY ||
+    "AIzaSyBMHk6IRgZG1S6eeoq1T7BTOSIaWnYxSSs",
+  authDomain:
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+    "solarladder-2d47a.firebaseapp.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "solarladder-2d47a",
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
+    "solarladder-2d47a.firebasestorage.app",
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "816197589445",
+  appId:
+    process.env.REACT_APP_FIREBASE_APP_ID ||
+    "1:816197589445:web:8909b5bd7d6636ba03d3ac",
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-8N3ECNDM8Z",
 };
 
 const app = initializeApp(firebaseConfig);
